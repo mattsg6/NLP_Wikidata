691907c8e5ff1286b5c13889aa81de2f568bdb79
Matthew Gilmore <mattsg@email.unc.edu>
2023-05-17 13:56:22 -0400

    openai price blocker

--- scripts/openai.py
+++ scripts/openai.py
@@ -1,14 +1,11 @@
-import os
-import openai
 import requests
-import sseclient
-import json
+from dotenv import dotenv_values
 
 endpoint = 'https://api.openai.com/v1/completions'
 
 input = {
     "model":"text-davinci-003",
-    "prompt":"Say this is a test",
+    "prompt":"Hello",
     "max_tokens":7,
     "temperature":0,
     "stream": True
@@ -16,13 +13,8 @@ input = {
 
 headers = {
     'Accept': 'text/event-stream',
-    'Authorization': 'Bearer ' + os.environ['OPENAI_API_KEY']
+    'Authorization': 'Bearer ' + dotenv_values('.env')['OPENAI_API_KEY']
 }
 
 response = requests.post(endpoint, stream = True, headers=headers, json=input)
-sseClient = sseclient.SSEClient(response)
-
-
-for event in sseClient.events():
-    if event.data != '[DONE]':
-        print(json.loads(event.data)['choices'][0]['text'], end='', flush=True)
+print(response.json())

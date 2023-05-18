28deffd14e2f63266d155369cda2fe4cf4b8980c
Matthew Gilmore <mattsg@email.unc.edu>
2023-05-18 11:33:59 -0400

    wiki data api

---
+++ scripts/wikidata.py
@@ -0,0 +1,38 @@
+import sys
+from SPARQLWrapper import SPARQLWrapper, JSON
+
+endpoint_url = "https://query.wikidata.org/sparql"
+
+query = """PREFIX wd: <http://www.wikidata.org/entity/>
+PREFIX wdt: <http://www.wikidata.org/prop/direct/>
+
+SELECT ?filmLabel WHERE {
+  ?film wdt:P31 wd:Q11424 ;
+        wdt:P57 wd:Q522057 ;
+        wdt:P161 wd:Q238464 ;
+        rdfs:label ?filmLabel .
+  FILTER (lang(?filmLabel) = "en")
+}
+"""
+
+
+def get_results(endpoint_url, query):
+    user_agent = "Wikidata-AI-app/%s.%s" % (sys.version_info[0], sys.version_info[1])
+    sparql = SPARQLWrapper(endpoint_url, agent=user_agent)
+    sparql.setQuery(query)
+    sparql.setReturnFormat(JSON)
+    return sparql.query().convert()
+
+
+results = get_results(endpoint_url, query)
+
+format = []
+for result in results["results"]["bindings"]:
+    keys = list(result.keys())
+    temp = {}
+    for k in keys:
+        temp[k] = result[k]['value']
+
+    format.append(temp)
+
+print(format)

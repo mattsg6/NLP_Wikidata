c5a4ae300d6683c18fc5e78c029cbdd784dedaa0
Matthew Gilmore <mattsg@email.unc.edu>
2023-05-19 09:39:02 -0400

    update

--- routes/api.py
+++ routes/api.py
@@ -14,23 +14,23 @@ def res():
     endpoint_url = "https://query.wikidata.org/sparql"
 
     query = """PREFIX wd: <http://www.wikidata.org/entity/>
-PREFIX wdt: <http://www.wikidata.org/prop/direct/>
+                PREFIX wdt: <http://www.wikidata.org/prop/direct/>
 
-SELECT ?film ?cinematographer WHERE {
-  ?films wdt:P31 wd:Q11424 ;
-        wdt:P57 wd:Q7374 ;
-        wdt:P344 ?cinematographers .
-  ?films rdfs:label ?film .
-  ?cinematographers rdfs:label ?cinematographer .
-  FILTER (lang(?film) = "en" && lang(?cinematographer) = "en")
-}
+                SELECT ?film ?cinematographer WHERE {
+                ?films wdt:P31 wd:Q11424 ;
+                        wdt:P57 wd:Q7374 ;
+                        wdt:P344 ?cinematographers .
+                ?films rdfs:label ?film .
+                ?cinematographers rdfs:label ?cinematographer .
+                FILTER (lang(?film) = "en" && lang(?cinematographer) = "en")
+                }
     """
 
     question = 'What are the names of all the films directed by Alfred Hitchcock and who was the cinematographer for each?'
 
     payload = wikidata_query(endpoint_url=endpoint_url, query=query)
 
-    return render_template('results.html', res = payload, question=question)
+    return render_template('results.html', res=payload, question=question)
 
 @api.route('/question', methods=['POST'])
 def process():
--- scripts/wikidata.py
+++ scripts/wikidata.py
@@ -1,21 +1,6 @@
 import sys
 from SPARQLWrapper import SPARQLWrapper, JSON
 
-endpoint_url = "https://query.wikidata.org/sparql"
-
-query = """PREFIX wd: <http://www.wikidata.org/entity/>
-PREFIX wdt: <http://www.wikidata.org/prop/direct/>
-
-SELECT ?filmLabel WHERE {
-  ?film wdt:P31 wd:Q11424 ;
-        wdt:P57 wd:Q522057 ;
-        wdt:P161 wd:Q238464 ;
-        rdfs:label ?filmLabel .
-  FILTER (lang(?filmLabel) = "en")
-}
-"""
-
-
 def get_results(endpoint_url, query):
     user_agent = "Wikidata-AI-app/%s.%s" % (sys.version_info[0], sys.version_info[1])
     sparql = SPARQLWrapper(endpoint_url, agent=user_agent)
